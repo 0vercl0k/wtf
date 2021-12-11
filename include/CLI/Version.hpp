829f463809a21900893c5aded1abf6d4fc95d528
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2021-12-11 14:04:37 -0800

    Squashed 'src/libs/CLI11/' content from commit 70f8072f
    git-subtree-dir: src/libs/CLI11
    git-subtree-split: 70f8072f9dd2292fd0b9f9e5f58e279f60483ed3

---
+++ include/CLI/Version.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2017-2021, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+// [CLI11:version_hpp:verbatim]
+
+#define CLI11_VERSION_MAJOR 2
+#define CLI11_VERSION_MINOR 1
+#define CLI11_VERSION_PATCH 2
+#define CLI11_VERSION "2.1.2"
+
+// [CLI11:version_hpp:end]
