c9a19237dc0a5c62eb649bacee0e874bf6f3bf36
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2021-12-19 15:05:54 -0800

    clang13

--- src/build/build-asan.sh
+++ src/build/build-asan.sh
@@ -1,5 +1,5 @@
-export CC=clang-11
-export CXX=clang++-11
+export CC=clang-13
+export CXX=clang++-13
 export CFLAGS='-fsanitize=address'
 export CXXFLAGS='-fsanitize=address'
 cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja && cmake --build .
