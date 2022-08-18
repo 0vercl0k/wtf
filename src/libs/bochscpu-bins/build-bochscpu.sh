73468bb6528de5bb6ee47da58d26967169510952
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2022-08-17 21:40:15 -0700

    inject changes

--- src/libs/bochscpu-bins/build-bochscpu.sh
+++ src/libs/bochscpu-bins/build-bochscpu.sh
@@ -10,26 +10,26 @@ git clone https://github.com/yrp604/bochscpu
 git clone https://github.com/yrp604/bochscpu-ffi
 
 cd bochscpu-build
-bash prep.sh && cd bochs && sh .conf.cpu && make || true
+bash prep.sh && cd Bochs/bochs && sh .conf.cpu && make || true
 
 # Remove old files in bochscpu.
-rm -rf ../../bochscpu/bochs
-rm -rf ../../bochscpu/libs
+rm -rf ../../../bochscpu/bochs
+rm -rf ../../../bochscpu/libs
 
 # Create the libs directory where we stuff all the libs.
-mkdir ../../bochscpu/lib
-cp cpu/libcpu.a ../../bochscpu/lib/libcpu.a
-cp cpu/fpu/libfpu.a ../../bochscpu/lib/libfpu.a
-cp cpu/avx/libavx.a ../../bochscpu/lib/libavx.a
-cp cpu/cpudb/libcpudb.a ../../bochscpu/lib/libcpudb.a
+mkdir ../../../bochscpu/lib
+cp cpu/libcpu.a ../../../bochscpu/lib/libcpu.a
+cp cpu/fpu/libfpu.a ../../../bochscpu/lib/libfpu.a
+cp cpu/avx/libavx.a ../../../bochscpu/lib/libavx.a
+cp cpu/cpudb/libcpudb.a ../../../bochscpu/lib/libcpudb.a
 make all-clean
 
 # Now we want to copy the bochs directory over there.
 cd ..
-mv bochs ../bochscpu/bochs
+mv bochs ../../bochscpu/bochs
 
 # Now its time to build it.
-cd ../bochscpu-ffi
+cd ../../bochscpu-ffi
 
 cargo clean
 cargo build
