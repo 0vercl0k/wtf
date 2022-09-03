67beeb4ef0297253177397f8505265ac12cd55eb
huwwp <hpigott@gmail.com>
2022-09-04 03:25:55 +1000

    Generate coverage file with Ghidra (#128)

---
+++ scripts/gen_coveragefile_ghidra.py
@@ -0,0 +1,28 @@
+#Output a module coverage file for wtf
+#@author huwwp
+#@category fuzzing
+
+from ghidra.program.model.block import BasicBlockModel
+import json
+
+program = getCurrentProgram()
+output_path = program.getExecutablePath().rsplit("/",1)[0]
+program_name = program.getName().rsplit(".",1)[0]
+base_address = program.getImageBase().getOffset()
+block_iterator = BasicBlockModel(program).getCodeBlocks(monitor)
+block = block_iterator.next()
+
+address_list = []
+
+while block:
+    address_list.append(block.minAddress.getOffset() - base_address)
+    block = block_iterator.next()
+
+json_object = {
+    'name': program_name,
+    'addresses': address_list
+}
+
+file = open(output_path + "/" + program_name + ".cov", "w")
+json.dump(json_object, file)
+file.close()
