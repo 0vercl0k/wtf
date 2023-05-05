41f72057acbadc5ef5b8f2be9a1805204a8c3fee
Leo Adrien <47684814+australeo@users.noreply.github.com>
2023-05-05 11:27:14 +1000

    Update gen_coveragefile_binja.py
    Switched filename parsing to Path(x).stem and tidied up imports.

--- scripts/gen_coveragefile_binja.py
+++ scripts/gen_coveragefile_binja.py
@@ -2,13 +2,13 @@
 
 @australeo - 2023
 """
-import json
-import os
-from binaryninja import PluginCommand, basicblock, interaction
+from json import dump
+from pathlib import Path
+from binaryninja import PluginCommand, interaction
 
 def generate_coverage_file(bv):
     # bv.file.filename: 'C:/path/to/binary.bndb'
-    name = bv.file.filename.split("/")[-1].split('.')[0]
+    name = Path(bv.file.filename).stem
 
     bb_list = []
 
@@ -23,7 +23,7 @@ def generate_coverage_file(bv):
     output_file = interaction.get_save_filename_input("Filename: ", ".cov", name + ".cov")
 
     with open(output_file, "w", encoding="utf-8") as file:
-        json.dump(json_object, file)
+        dump(json_object, file)
 
 def register_plugin():
     PluginCommand.register("Generate WTF coverage file", \
